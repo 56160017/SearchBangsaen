0126d4cc7c2ecf2b95dacbf23e85f92f27afee52
56160017 <se56160017@gmail.com>
2017-01-09 10:04:16 +0700

    first project -p

--- app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
+++ app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
@@ -1,13 +1,41 @@
 package com.buu.se.searchbangsaen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.buu.se.searchbangsaen.searchcategories.RestaurantSearchActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tvTest)
+    TextView tvTest;
+    @BindView(R.id.btnSubmit)
+    Button btnSubmit;
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
