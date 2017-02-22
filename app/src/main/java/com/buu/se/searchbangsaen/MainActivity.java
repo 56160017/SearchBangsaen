d4baa1c67b0d8f61352ae60b5fc9ff6fe5fad4ea
56160017 <se56160017@gmail.com>
2017-02-22 13:50:50 +0700

    draw -p

--- app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
+++ app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
@@ -1,35 +1,36 @@
 package com.buu.se.searchbangsaen;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.buu.se.searchbangsaen.searchcategories.RestaurantSearchActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @BindView(R.id.tvTest)
-    TextView tvTest;
-    @BindView(R.id.btnSubmit)
-    Button btnSubmit;
-    @BindView(R.id.activity_main)
-    RelativeLayout activityMain;
+    @BindView(R.id.btnRestaurant) ImageButton btnRestaurant;
+    @BindView(R.id.btnHotel) ImageButton btnHotel;
+    @BindView(R.id.btnCoffee) ImageButton btnCoffee;
+    @BindView(R.id.btnGift) ImageButton btnGift;
+    @BindView(R.id.btnWS) ImageButton btnWS;
+    @BindView(R.id.btnFestival) ImageButton btnFestival;
+    @BindView(R.id.activity_main) LinearLayout activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
@@ -37,5 +38,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
---
+++ app/src/main/java/com/buu/se/searchbangsaen/StartAppActivity.java
@@ -0,0 +1,90 @@
+package com.buu.se.searchbangsaen;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.buu.se.searchbangsaen.searchcategories.RestaurantSearchActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartAppActivity extends AhoyOnboarderActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_start_app);
+        AhoyOnboarderCard ahoyOnboarderCardFirst = new AhoyOnboarderCard("Search Bangsean", "Easy Sreach Easy Go!!", R.drawable.ic_coconut);
+        ahoyOnboarderCardFirst.setTitleTextSize(40);
+        ahoyOnboarderCardFirst.setDescriptionTextSize(25);
+        AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("คุณกำลังหาอะไร", "จะเที่ยวไหนก็หาเจอ \nจะไปไหนก็รู้ก่อนใคร", R.drawable.ic_spend);
+        ahoyOnboarderCard1.setTitleTextSize(40);
+        ahoyOnboarderCard1.setDescriptionTextSize(25);
+        AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("คุณจะไปยังไง", "มีแผนที่ในการนำทาง \nอยู่ตรงไหนก็ไปถูก", R.drawable.ic_map_of_road);
+        ahoyOnboarderCard2.setTitleTextSize(40);
+        ahoyOnboarderCard2.setDescriptionTextSize(25);
+        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("เริ่มใช้งานกันเลย!!", "", R.drawable.ic_reward);
+        ahoyOnboarderCard3.setTitleTextSize(40);
+      //  ahoyOnboarderCard2.setDescriptionTextSize(25);
+
+        ahoyOnboarderCardFirst.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard1.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard2.setBackgroundColor(R.color.black_transparent);
+        ahoyOnboarderCard3.setBackgroundColor(R.color.black_transparent);
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+        pages.add(ahoyOnboarderCardFirst);
+        pages.add(ahoyOnboarderCard1);
+        pages.add(ahoyOnboarderCard2);
+        pages.add(ahoyOnboarderCard3);
+        for (AhoyOnboarderCard page : pages) {
+            page.setTitleColor(R.color.white);
+            page.setDescriptionColor(R.color.grey_200);
+        }
+       // ahoyOnboarderCardFirst.setIconLayoutParams(500,500,50,0,0,0);
+
+        setFinishButtonTitle("Get Started");
+
+        showNavigationControls(false);
+
+        //setGradientBackground();
+       // setImageBackground(R.drawable.bg_landing);
+          //  setImageBackground(R.drawable.bg_landing);
+
+        List<Integer> colorList = new ArrayList<>();
+        colorList.add(R.color.solid_frist);
+        colorList.add(R.color.solid_one);
+        colorList.add(R.color.solid_two);
+        colorList.add(R.color.solid_frist);
+        setColorBackground(colorList);
+
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/DB_ThaiText/DB_ThaiText_Bold_X.ttf");
+
+        setFont(face);
+//        setInactiveIndicatorColor(R.color.grey_600);
+//        setActiveIndicatorColor(R.color.white);
+        setOnboardPages(pages);
+    }
+
+    @Override
+    public void setFinishButtonDrawableStyle(Drawable res) {
+        super.setFinishButtonDrawableStyle(res);
+
+    }
+
+
+    @Override
+    public void onFinishButtonPressed() {
+       // Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
