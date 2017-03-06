137ddf4f4414fb5e0f437ba5117a8a6732db576f
Sattra Phosri <s56160415@gmail.com>
2017-03-06 10:01:47 +0700

    yaya

--- app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
+++ app/src/main/java/com/buu/se/searchbangsaen/MainActivity.java
@@ -2,27 +2,33 @@ package com.buu.se.searchbangsaen;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.buu.se.searchbangsaen.searchcategories.activity.RestaurantSearchActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btnRestaurant) ImageButton btnRestaurant;
-    @BindView(R.id.btnHotel) ImageButton btnHotel;
-    @BindView(R.id.btnCoffee) ImageButton btnCoffee;
-    @BindView(R.id.btnGift) ImageButton btnGift;
-    @BindView(R.id.btnWS) ImageButton btnWS;
-    @BindView(R.id.btnFestival) ImageButton btnFestival;
+
+    @BindView(R.id.civ_search) CircleImageView civSearch;
+    @BindView(R.id.tv_login) TextView tvLogin;
+    @BindView(R.id.tv_report) TextView tvReport;
+    @BindView(R.id.btn_Restaurant) CircleImageView btnRestaurant;
+    @BindView(R.id.img_Hotel) CircleImageView imgHotel;
+    @BindView(R.id.btn_Coffee) CircleImageView btnCoffee;
+    @BindView(R.id.img_Gift) CircleImageView imgGift;
+    @BindView(R.id.btn_Ws) CircleImageView btnWs;
+    @BindView(R.id.img_Festival) CircleImageView imgFestival;
     @BindView(R.id.activity_main) LinearLayout activityMain;
 
     @Override
@@ -30,15 +36,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        tvLogin.setPaintFlags(tvLogin.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        tvReport.setPaintFlags(tvReport.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        tvLoginOnClickListener();
         btnRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            //    Intent intent = new Intent(getApplicationContext(), CardRestaurantSearchActivity.class);
+                //    Intent intent = new Intent(getApplicationContext(), CardRestaurantSearchActivity.class);
                 Intent intent = new Intent(getApplicationContext(), RestaurantSearchActivity.class);
                 startActivity(intent);
             }
         });
 
+    }
+
+    private void tvLoginOnClickListener() {
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
